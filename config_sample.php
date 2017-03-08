ebbf0be8c56075a3348c83e8c88bc2f809da5aab
bernorieder <berno.rieder@gmail.com>
2017-03-08 11:07:18 +0100

    all new!

--- config_sample.php
+++ config_sample.php
@@ -1,5 +1,20 @@
 <?php
 
-$apikey = "put your API key here and rename the file to config.php";	
+$inputfile = "thelistcontainingtheurls.tab";							// filename for the input file, CSV or TSV
+
+$urlcolumn = "imageurl";												// name of the column that contains the image URL? 
+$csvdelimiter = "\t"; 													// the column delimiter
+
+// three folders the script needs, create in the same directory and make sure they can be written to
+$datadir = getcwd() . "/data/";
+$jsondir = getcwd() . "/cache/";
+$outputsdir = getcwd() . "/outputs/";
+
+$apikey = "AIzaSyDHU2an8sG-k2S5tzhlw7dvqYw0mEk5HLM";					// your api key
+
+ignore_user_abort(false);
+set_time_limit(3600*5);
+ini_set("memory_limit","100M");
+ini_set("error_reporting",1);	
 	
 ?>
